c00ca665a927e697caa68660a8c55acfb5c8db43
Ayush Rajpal <ayushrajpal2021@gmail.com>
2025-07-24 16:23:58 +0530

    setup

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Ayush Rajpal on 24/07/25.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
